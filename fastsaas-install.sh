95ef738956a3f79d2168ee7ab87f80a688a79989
Matt Peterson <Matt.Peterson@quest.com>
2021-04-15 13:03:00 -0600

    Update fastsaas-install.sh

--- fastsaas-install.sh
+++ fastsaas-install.sh
@@ -8,8 +8,20 @@ if (( $EUID != 0 )); then
 fi
 
 # install necessary prerequites
-apt update 
-apt -y install apache2 php
+#wait for the dpkg lock to be released
+while sudo fuser /var/lib/dpkg/lock >/dev/null 2>&1 ; do
+    sleep 1
+done
+while sudo fuser /var/lib/apt/lists/lock >/dev/null 2>&1 ; do
+    sleep 1
+done
+if [ -f /var/log/unattended-upgrades/unattended-upgrades.log ]; then
+    while sudo fuser /var/log/unattended-upgrades/unattended-upgrades.log >/dev/null 2>&1 ; do
+        sleep 1
+    done
+fi
+apt-get update 
+apt-get -y install apache2 php
 
 # copy files to apache html directory
 
